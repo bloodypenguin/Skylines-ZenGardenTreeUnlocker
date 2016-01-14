5f6712ce58b7fbb7e443f151e3e336a044e9f93d
Egor Aralov <earalov@thumbtack.net>
2016-01-15 01:46:40 +0600

    Add to source control

---
+++ ZenGardenTreeUnlocker/Util.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using ColossalFramework.UI;
+using UnityEngine;
+
+namespace ZenGardenTreeUnlocker
+{
+    public static class Util
+    {
+        public static Texture2D LoadTextureFromAssembly(string path, bool readOnly = true)
+        {
+            try
+            {
+                var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                using (var textureStream = assembly.GetManifestResourceStream(path))
+                {
+                    return LoadTextureFromStream(readOnly, textureStream);
+                }
+            }
+            catch (Exception e)
+            {
+                UnityEngine.Debug.LogException(e);
+                return null;
+            }
+        }
+
+        public static UITextureAtlas CreateAtlas(Texture2D[] sprites)
+        {
+            UITextureAtlas atlas = new UITextureAtlas();
+            atlas.material = new Material(GetUIAtlasShader());
+
+            Texture2D texture = new Texture2D(0, 0);
+            Rect[] rects = texture.PackTextures(sprites, 0);
+
+            for (int i = 0; i < rects.Length; ++i)
+            {
+                Texture2D sprite = sprites[i];
+                Rect rect = rects[i];
+
+                UITextureAtlas.SpriteInfo spriteInfo = new UITextureAtlas.SpriteInfo();
+                spriteInfo.name = sprite.name;
+                spriteInfo.texture = sprite;
+                spriteInfo.region = rect;
+                spriteInfo.border = new RectOffset();
+
+                atlas.AddSprite(spriteInfo);
+            }
+            atlas.material.mainTexture = texture;
+            return atlas;
+        }
+
+        private static Shader GetUIAtlasShader()
+        {
+            return UIView.GetAView().defaultAtlas.material.shader;
+        }
+
+        private static Texture2D LoadTextureFromStream(bool readOnly, Stream textureStream)
+        {
+            var buf = new byte[textureStream.Length]; //declare arraysize
+            textureStream.Read(buf, 0, buf.Length); // read from stream to byte array
+            textureStream.Close();
+            var tex = new Texture2D(2, 2, TextureFormat.ARGB32, false);
+            tex.LoadImage(buf);
+            tex.Apply(false, readOnly);
+            tex.name = Guid.NewGuid().ToString();
+            return tex;
+        }
+    }
+}
---
+++ ZenGardenTreeUnlocker/ZenGardenTreeUnlocker.cs
@@ -0,0 +1,62 @@
+﻿using System.Reflection;
+using ColossalFramework;
+using ColossalFramework.Globalization;
+using ICities;
+using UnityEngine;
+
+namespace ZenGardenTreeUnlocker
+{
+    public class ZenGardenTreeUnlocker : LoadingExtensionBase, IUserMod
+    {
+        public string Name => "Zen Garden Cherry Blossom Unlocker";
+
+        public string Description => "Unlocks for placing the Cherry Blossom tree that is bundled with Zen Garden";
+
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            base.OnLevelLoaded(mode);
+            var prefab = PrefabCollection<TreeInfo>.FindLoaded("CherryTree01");
+            if (prefab == null)
+            {
+                UnityEngine.Debug.LogWarning("CherryTree01 wasn't found");
+                return;
+            }
+            prefab.m_availableIn = ItemClass.Availability.All;
+            var field = typeof (PrefabInfo).GetField("m_UICategory", BindingFlags.Instance | BindingFlags.NonPublic);
+            field.SetValue(prefab, "BeautificationProps");
+            var thumb = Util.LoadTextureFromAssembly($"{typeof (ZenGardenTreeUnlocker).Name}.thumb.png", false);
+            var tooltip = Util.LoadTextureFromAssembly($"{typeof(ZenGardenTreeUnlocker).Name}.tooltip.png", false);
+            var atlas = Util.CreateAtlas(new[] {thumb, tooltip});
+            prefab.m_Atlas = atlas;
+            prefab.m_Thumbnail = thumb.name;
+            prefab.m_InfoTooltipAtlas = atlas;
+            prefab.m_InfoTooltipThumbnail = tooltip.name;
+
+
+            var localeField = typeof(LocaleManager).GetField("m_Locale", BindingFlags.NonPublic | BindingFlags.Instance);
+            var locale = (Locale)localeField.GetValue(SingletonLite<LocaleManager>.instance);
+            var key = new Locale.Key() { m_Identifier = "TREE_TITLE", m_Key = prefab.name };
+            if (!locale.Exists(key))
+            {
+                locale.AddLocalizedString(key, "Zen Garden Cherry Blossom");
+            }
+            key = new Locale.Key() { m_Identifier = "TREE_DESC", m_Key = prefab.name };
+            if (!locale.Exists(key))
+            {
+                locale.AddLocalizedString(key, "A Cherry Blossom tree that is bundled with Zen Garden");
+            }
+
+            switch (mode)
+            {
+                case LoadMode.NewGame:
+                case LoadMode.LoadGame:
+                    GameObject.Find("BeautificationPropsPanel").GetComponent<BeautificationPanel>().RefreshPanel();
+                    break;
+                case LoadMode.NewMap:
+                case LoadMode.LoadMap:
+                    GameObject.Find("ForestPanel").GetComponent<ForestGroupPanel>().RefreshPanel();
+                    break;
+            }
+        }
+    }
+}
